a75a9204157b5dd01a89b62019318e1f286b8c81
Daniel Chang'masa <danchangmasa@gmail.com>
2018-03-26 01:14:30 +0300

    Add files via upload

--- src/main/java/tk/macoz/demo/controller/MyController.java
+++ src/main/java/tk/macoz/demo/controller/MyController.java
@@ -1,7 +1,9 @@
 package tk.macoz.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import tk.macoz.demo.bean.Country;
 import tk.macoz.demo.service.ICountryService;
@@ -13,7 +15,8 @@ public class MyController {
     @Autowired
     private ICountryService countryService;
 
-    @RequestMapping("/countries")
+    @RequestMapping(value="/countries",  method= RequestMethod.GET,
+            produces= MediaType.APPLICATION_XML_VALUE)
     public List<Country> listCountries() {
 
         return countryService.findAll();
